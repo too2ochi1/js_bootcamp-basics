821c8d52a153f42a98526ba36c98acc8ba01d81c
Jason Herbert <herbertj289@gmail.com>
2024-02-28 20:14:28 +0100

    A code that shows how booleans and comparison operations work. Also includes a project for ticket discounting that showed how the conditional statement if can be used in basic form

---
+++ boolean.js
@@ -0,0 +1,24 @@
+// === stands as a "quality operator"
+// !== stands as "no equal operator"
+// < stands as "less than operator"
+// > stands as "greater than operator"
+// <= stands as "less than or equal to operator"
+// >= stands as "greater than or equal to operator"
+let child_age = 5
+let senior_age = 64
+
+if (child_age <= 7) {
+    console.log ('Welcome, little one! You get a 10% youth discount!')
+}
+
+if (child_age > 7) {
+    console.log ('You do not qualify for the discount. Pay your normal ticket fee')
+}
+
+if (senior_age >= 65) {
+    console.log ('You made it this far. You get a 10% OG discount!')
+}
+
+if (senior_age < 65) {
+    console.log ('You do not qualify for the discount. Pay your normal ticket fee')
+}
