0cdef64f381d2342af38c9e67e16e0d1f011bfbd
Jason Herbert <herbertj289@gmail.com>
2024-02-28 09:42:27 +0100

    Rules about variables,may be subject to change/update as time goes on

---
+++ variables_and_its_rules.js
@@ -0,0 +1,5 @@
+//1. You can't define a variable more than once, you can only update the value of an already defined variable
+//2. There are rules guiding naming of variables 
+//3. You can't name a variable starting with a number, it has to start with two symbols or a letter
+//4. You can't use some specific special characters in naming a variable, you can only use the dollar sign or underscore sign to name a variable
+//5. Variable names cannot be reserved keywords eg "let"
