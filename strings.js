62a3066e78fa76569cca418b766e5f7ebd0a078f
Jason Herbert <herbertj289@gmail.com>
2024-02-28 08:49:27 +0100

    Sike! Had one more thing, but fr I'm done now

--- strings.js
+++ strings.js
@@ -1,4 +1,4 @@
-let first_name = 'Jason'
-let last_name = 'Herbert'
-let full_name = first_name + ' ' + last_name
-console.log(full_name)
+let city = 'Lagos'
+let country = 'Nigeria'
+let location = city + ',' + ' ' + country
+console.log(location)
