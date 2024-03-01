6f3358a7db02771aae1ab25baeb0083ef4e43fdb
Jason Herbert <herbertj289@gmail.com>
2024-03-01 13:39:51 +0100

    more on scopes, how to define variables more than once by manipulating scope structures

---
+++ scope_2.js
@@ -0,0 +1,19 @@
+// Global (name)
+  // Local (name)
+    // Local
+  // Local
+ 
+ let name = 'Jason'
+ 
+if (true) {
+    let name = 'Mike'
+
+    if (true) {
+        name = 'Jasper'
+        console.log(name)
+    }
+}
+
+if (true) {
+    console.log(name) 
+}
